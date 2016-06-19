b1b9399f0cc60166fbcda4541daa045931c74d8e
amirali <yones.gholami45@gmail.com>
2016-06-19 15:11:19 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,26 +251,14 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {214795464,180252366},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[MeGa shield v2.9
+    about_text = [[MeGa  v2.9
 An advanced administration bot based on TG-CLI written in Lua
 
-Github:
-https://github.com/hafez16/mega-shield
 
 Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
-
-Special thanks to
-SEEDTEAM
-Hextor team
-Magic team
-Avira team
-
+@Mr_Barbod [Developer
 Our channels
 @shieldTM [persian]
 ]],
